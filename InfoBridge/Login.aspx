16fa355bfbc8cca7a5ce7a7fb12e0307048b1901
Tanuja167 <104304824+Tanuja167@users.noreply.github.com>
2024-03-09 13:48:59 +0530

    Add project files.

---
+++ InfoBridge/Login.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="InfoBridge.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblhead" runat="server" BackColor="#FFFF66" ForeColor="#33CC33" style="text-align: center" Text="Login Form"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="lblusername" runat="server" Text="Username"></asp:Label>
+            <asp:TextBox ID="txtusername" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="lblpassword" runat="server" Text="Password"></asp:Label>
+            <asp:TextBox ID="txtpassword" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Login" />
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ InfoBridge/Login.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using Microsoft.AspNet.FriendlyUrls;
+using System.Web.Services.Description;
+using System.Windows;
+
+namespace InfoBridge
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           if(Label1.Text == "Login Sucessfully")
+            {
+                Response.Redirect("~/Default.aspx");
+            }
+            else
+            {
+                MessageBox.Show("Invalid exception");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = txtusername.Text;
+            string password = txtpassword.Text;
+            string validusername = "admin";
+            string validpassword = "admin"; 
+
+            if(username == validusername && password == validpassword)
+            {
+                Label1.Text = "Login Sucessfully";
+                Label1.ForeColor = System.Drawing.Color.Green;
+                Response.Redirect("~/Default.aspx");
+               
+            }
+            else
+            {
+                Response.Write("Username & Password Not Matched");
+            }
+            
+        }
+    }
+}
